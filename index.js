6d7b702837a2c69ad7d020dac152f204b45bc317
Aliyv019 <tahiraliyev2006@gmail.com>
2024-08-02 16:54:25 +0400

    added hover

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ list.innerHTML=""
 
 const search_result=document.querySelector('.search_result')
 
+
 search_btn.addEventListener('click',()=>{if(input.value.trim()!=""){searchclick()}})
-input.addEventListener('keydown',event=>{
-    if(event.key=="Enter" && input.value.trim()!=""){searchclick()}
-})
+    input.addEventListener('keydown',event=>{
+        if(event.key=="Enter" && input.value.trim()!=""){searchclick()}
+    })
 
 async function searchclick(){
     list.innerHTML=""
@@ -18,15 +19,21 @@ async function searchclick(){
         const data=await response.json()
         for(let i=0;i<5;i++){
             try {
-                const response2=await fetch(`https://www.omdbapi.com/?apikey=7704dd86&i=${data.Search[i].imdbID}&plot=short`)
+                const response2=await fetch(`https://www.omdbapi.com/?apikey=7704dd86&i=${data.Search[i].imdbID}&plot=full`)
                 const data2=await response2.json()
                 const ListItem=document.createElement('li')
                 ListItem.innerHTML=`
+                        <div class="movie_front movie">
                         <img src="${data2.Poster}">
                         <h2>${data2.Title}(${data2.Year})</h2>
                         <p>IMDB Rating:${data2.imdbRating}</p>
                         <p>Release Date:${data2.Released}</p>
-                        <p>${data2.Plot}</p>`
+                        <p>${data2.Plot}</p>
+                        </div>
+                        <div class="movie_back movie">
+                        <h2>${data2.Title}(${data2.Year})</h2>
+                        <p>${data2.Plot}</p>
+                        </div>`
                 list.appendChild(ListItem)
                 console.log(data2);
             } catch (error) {
